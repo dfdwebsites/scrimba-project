d43bf63705b995d2529b6b7a8bc7b544751cc48e
drd4games <tsenisda@gmail.com>
2022-03-17 13:23:18 +0200

    blackjack start

--- src/games/cards/BlackJack.js
+++ src/games/cards/BlackJack.js
@@ -1,43 +1,99 @@
-import React from "react";
+import React, {useState} from "react";
 
 
 export default function BlackJack(){
-    let deckId
-
+    
+    const [deckId, setDeckId] = useState(null)
+    const [gameStarted,setGameStarted] = useState(false)
+    const [playerHand,setPlayerHand] = useState([])
+    const [croupier,setCroupier] = useState([])
     React.useEffect(()=>{
 
         
     })
+
+
+    function getValue(value){
+        switch(value){
+            case "2": value=2
+            break
+            case "3": value=3
+            break
+            case "4": value=4
+            break
+            case "5": value=5
+            break
+            case "6": value=6
+            break
+            case "7": value=7
+            break
+            case "8": value=8
+            break
+            case "9": value=9
+            break
+            case "10": value=10
+            break
+            case "JACK": value=10
+            break
+            case "QUEEN": value=10
+            break
+            case "KING": value=10
+            break
+            case "ACE": value=11
+            break
+        }
+    }
+
+    
+    function test(){
+         drawTwo(playerHand)
+         getTotal(playerHand)
+        }
+        function getTotal(arr){
+            arr.map(val=>getValue(val))
+            let total = arr.reduce((a,b)=>{
+               return a+b
+            })
+            console.log(total)   
+        }
     function getDeck(){
         fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6")
         .then(res=>res.json())
         .then(data=>{
             console.log(data.deck_id)
-            deckId = data.deck_id
+            setDeckId(data.deck_id)
+            setGameStarted(true)
         })
     }
-    function drawTwo(){
+   async function drawTwo(hand){
         fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
         .then(res=>res.json())
         .then(data=>{
             console.log(data)
+            hand.push(data.cards[0])
+            hand.push(data.cards[1])
             
         })
     }
-    function draw(){
+    function draw(hand){
         fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
         .then(res=>res.json())
         .then(data=>{
             console.log(data)
-            
+            hand.push(data.cards[0])
         })   
     }
 
     return (<>
     <div className="blackJack-container">
-        <button onClick={getDeck}>get Deck</button>
-        <button onClick={draw}>draw 1</button>
-        <button onClick={drawTwo}>draw 2</button>
+        <button disabled={gameStarted} onClick={getDeck}>Start Game</button>
+        {gameStarted &&<div>
+            <h3>Croupier</h3>
+            <div></div>
+            <button onClick={draw}>draw 1</button>
+            <button onClick={test}>test</button>
+            <button onClick={drawTwo}>draw 2</button>
+        </div>}
     </div>
     </>)
 }
--- src/games/cards/Cards.js
+++ src/games/cards/Cards.js
@@ -7,12 +7,11 @@ export default function Cards(){
     return (
         <>
         <Navbar />
-        <div className="snake-page-options black">
+        <div className="allCards">
 
             <Link className="black-btn" to="/games/cards/blackJack"> BlackJack </Link>
             <Link className="uno-btn" to="/games/cards/uno">UNO</Link>
             <Link className="war-btn"to="/games/cards/war"> War </Link>
-        
         
         </div>
         </>
